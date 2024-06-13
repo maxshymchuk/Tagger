6061989c5834ca77474cd20586c6cbf1387e9789
Max Shymchuk <octorix@mail.ru>
2024-06-13 21:24:47 +0200

    test: google oauth

--- client/modules/auth.js
+++ client/modules/auth.js
@@ -6,11 +6,6 @@ closeAuthMenu.addEventListener('click', closeMenu);
 // temp
 let isAuth = false;
 
-function auth() {
-    isAuth = true;
-    window.dispatchEvent(new Event('load'));
-}
-
 function openMenu() {
     authMenu.style.transform = 'translateY(-100%)';
 }
@@ -22,6 +17,72 @@ function closeMenu() {
 
 async function init() {
     return isAuth;
+}
+
+const CLIENT_ID = '700459303665-591ui2giotet3r0shuoe6uerktsqsu87.apps.googleusercontent.com';
+const REDIRECT_URI = 'https://maxshymchuk.github.io';
+
+const AUTH_URL = 'https://accounts.google.com/o/oauth2/v2/auth';
+const TOKEN_URL = 'https://oauth2.googleapis.com/token';
+const SCOPE = 'https://www.googleapis.com/auth/drive.metadata.readonly';
+
+document.getElementById('auth_btn').addEventListener('click', () => {
+    const authUrl = `${AUTH_URL}?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=token&scope=${encodeURIComponent(SCOPE)}&include_granted_scopes=true`;
+
+    console.log(authUrl)
+
+    const width = 500;
+    const height = 600;
+    const left = (screen.width / 2) - (width / 2);
+    const top = (screen.height / 2) - (height / 2);
+
+    const authWindow = window.open(authUrl, 'GoogleAuth', `width=${width},height=${height},top=${top},left=${left}`);
+
+    window.addEventListener('message', async (event) => {
+        if (event.origin !== window.location.origin) {
+            return;
+        }
+
+        const { code } = event.data;
+        if (code) {
+            const token = await fetchToken(code);
+            console.log('Access Token:', token.access_token);
+
+            const userInfo = await fetchUserInfo(token.access_token);
+            console.log('User Info:', userInfo);
+
+            isAuth = true;
+            window.dispatchEvent(new Event('load'));
+            authWindow.close();
+        }
+    }, false);
+});
+
+async function fetchToken(code) {
+    const response = await fetch(TOKEN_URL, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+            code: code,
+            client_id: CLIENT_ID,
+            client_secret: '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',
+            redirect_uri: REDIRECT_URI,
+            grant_type: 'authorization_code'
+        })
+    });
+
+    return response.json();
+}
+
+async function fetchUserInfo(token) {
+    const response = await fetch('https://www.googleapis.com/oauth2/v2/userinfo', {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+    return response.json();
 }
 
 export {
